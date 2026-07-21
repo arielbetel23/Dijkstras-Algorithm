0e013455b44922bf1391e420f6b6d52ea96706ba
Ariel Betel <arielbetel7@gmail.com>
2026-07-22 01:40:01 +0300

    added private to neighbors parameter in node class

--- src/Node.java
+++ src/Node.java
@@ -5,7 +5,7 @@ public class Node {
     private int id;
     private int distance;
      private boolean visited;
-     List<Edge> neighbors;
+     private List<Edge> neighbors;
 
      public Node(int id){
          this.id = id;
