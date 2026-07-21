d3dba67c0a53e1f1480179bab82d7538b6b289bd
Ariel Betel <arielbetel7@gmail.com>
2026-07-22 01:31:14 +0300

    created constructor for Edge class

--- src/Edge.java
+++ src/Edge.java
@@ -1,4 +1,9 @@
 public class Edge {
     private Node target;
     private int weight;
+
+    public Edge(Node target, int weight){
+        this.target = target;
+        this.weight = weight;
+    }
 }
