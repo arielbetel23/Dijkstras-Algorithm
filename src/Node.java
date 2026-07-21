49d6a69c49269a039932e5add1ab3afe513d4a76
Ariel Betel <arielbetel7@gmail.com>
2026-07-22 01:33:02 +0300

    added constructor for Node class

--- src/Node.java
+++ src/Node.java
@@ -5,4 +5,11 @@ public class Node {
     private int distance;
      private boolean visited;
      List<Edge> neighbors;
+
+     public Node(int id, int distance, boolean visited, List<Edge> neighbors){
+         this.id = id;
+         this.distance = distance;
+         this.visited = visited;
+         this.neighbors = neighbors;
+     }
 }
